3d530732a3190dd8338df4c998fcc74204039920
otolba <omar16tolba@gmail.com>
2024-11-12 15:27:46 -0800

    Meep Meep Set up plus beginning of test Auto

---
+++ MeepMeepTesting2/src/main/java/com/example/meepmeeptesting2/MeepMeepTesting2.java
@@ -0,0 +1,34 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting2 {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(50, 60, Math.toRadians(180), Math.toRadians(180), 13.669840915663318)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0.5, 70, Math.toRadians(270)))
+                .lineToY(47)
+                .splineTo(new Vector2d(-48, 30), Math.toRadians(270))
+//                .turn(Math.toRadians(90))
+//                .lineToX(0)
+//                .turn(Math.toRadians(90))
+//                .lineToY(0)
+//                .turn(Math.toRadians(90))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_LIGHT)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
