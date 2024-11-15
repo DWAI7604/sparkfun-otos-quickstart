a20c00c978ba8f2ff123d5768b01f215863fb13a
Edwin Chen <115381002+edwinSp1@users.noreply.github.com>
2024-11-15 11:37:11 -0800

    BLUE AUTO

--- MeepMeepTesting2/src/main/java/com/example/meepmeeptesting2/MeepMeepTesting2.java
+++ MeepMeepTesting2/src/main/java/com/example/meepmeeptesting2/MeepMeepTesting2.java
@@ -6,7 +6,23 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+
+
+
 public class MeepMeepTesting2 {
+    static double FORWARD = 3*Math.PI/2;
+    static double BACK = Math.PI/2;
+    static double LEFT = Math.PI;
+    static double RIGHT = 0;
+    private static final double pi = Math.PI;
+    private static final Pose2d SPECIMEN_POS = new Pose2d(0, 35, 3*pi/2);
+    private static final Pose2d OBSERVATION_POS = new Pose2d(-60, 50, BACK);
+    private static final Pose2d FIRST_SPECIMEN = new Pose2d(-52, 35, FORWARD);
+    private static final Pose2d SECOND_SPECIMEN = new Pose2d(-60, 35, FORWARD);
+    private static final Pose2d THIRD_SPECIMEN = new Pose2d(-70, 35, FORWARD);
+
+
+
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -15,21 +31,23 @@ public class MeepMeepTesting2 {
                 .setConstraints(50, 60, Math.toRadians(180), Math.toRadians(180), 13.669840915663318)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-5, 70, Math.toRadians(270)))
-                .lineToY(26)
-                .waitSeconds(2)
-                .lineToY(32)
-                .splineTo(new Vector2d(-48, 30), Math.toRadians(90))
-                .waitSeconds(2)
-                .lineToYLinearHeading(70, Math.toRadians(90))
-                .lineToYLinearHeading(60, Math.toRadians(270))
-                .splineTo(new Vector2d(-8, 26), Math.toRadians(270))
-                .lineToY(32)
-                .splineTo(new Vector2d(-48, 30), Math.toRadians(90))
-                .waitSeconds(2)
-                .lineToYLinearHeading(70, Math.toRadians(90))
-                .lineToYLinearHeading(60, Math.toRadians(270))
-                .splineTo(new Vector2d(-2, 26), Math.toRadians(270))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 72, Math.toRadians(270)))
+                                // move to specimen container
+                                .splineToLinearHeading(SPECIMEN_POS, pi)
+                                // grab first blue
+                                .splineToLinearHeading(FIRST_SPECIMEN, pi)
+                                .splineToLinearHeading(OBSERVATION_POS, pi)
+                                .splineToLinearHeading(SPECIMEN_POS, pi)
+                                // grab second blue
+                                .splineToLinearHeading(SECOND_SPECIMEN, pi)
+                                .splineToLinearHeading(OBSERVATION_POS, pi)
+                                .splineToLinearHeading(SPECIMEN_POS, pi)
+                                // grab last blue
+                                .splineToLinearHeading(THIRD_SPECIMEN, pi)
+                                .splineToLinearHeading(OBSERVATION_POS, pi)
+                                .splineToLinearHeading(SPECIMEN_POS, pi)
+
+
 //                .turn(Math.toRadians(90))
 //                .lineToX(0)
 //                .turn(Math.toRadians(90))
