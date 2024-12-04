d09771457c61dcab37040030eed11f8e542f0b44
otolba <omar16tolba@gmail.com>
2024-12-04 11:15:32 -0800

    imported telop class and set up test servo class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -1,4 +1,58 @@
 package org.firstinspires.ftc.teamcode;
 
-public class ServoTest {
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="SlideTele", group="Linear OpMode")
+public class ServoTest extends RobotLinearOpMode{
+    private Servo testServo;
+    private boolean bPressed;
+    private boolean aPressed;
+    private boolean xPressed;
+    @Override
+    public void runOpMode() {
+
+        testServo = hardwareMap.get(Servo.class, "testServo");
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            if (gamepad1.b && !bPressed) {
+                bPressed = true;
+            } else if (!gamepad1.b && bPressed) {
+                bPressed = false;
+            }
+
+            if (bPressed) {
+                bPressed = false;
+                testServo.setPosition(75 / 280);
+            }
+
+            if (gamepad1.a && !aPressed) {
+                aPressed = true;
+            } else if (!gamepad1.a && aPressed) {
+                aPressed = false;
+            }
+
+            if (aPressed) {
+                aPressed = false;
+                testServo.setPosition(60 / 280);
+            }
+
+            if (gamepad1.x && !xPressed) {
+                xPressed = true;
+            } else if (!gamepad1.x && xPressed) {
+                xPressed = false;
+            }
+
+            if (xPressed) {
+                xPressed = false;
+                testServo.setPosition(0);
+            }
+        }
+    }
 }
