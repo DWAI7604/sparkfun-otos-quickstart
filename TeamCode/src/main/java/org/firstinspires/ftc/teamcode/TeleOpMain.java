53c6c2b31013eacd1fc4590fdd6557b083edd90c
DrewEnnis <drewjennis@gmail.com>
2025-02-18 13:59:23 -0800

    Update TeleOpMain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -473,8 +473,8 @@ public class TeleOpMain extends RobotLinearOpMode {
 //            }
 
             if (gamepad2.right_bumper) {
-                slideUpTop.setPower(0.8);
-                slideUpBottom.setPower(0.8);
+                slideUpTop.setPower(-0.8);
+                slideUpBottom.setPower(-0.8);
             }
             else{
                 slideUpTop.setPower(0);
